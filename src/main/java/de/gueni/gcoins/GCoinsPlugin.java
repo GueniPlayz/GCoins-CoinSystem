25f7b6a42cc7180d515124f974e68aba68d98ca3
BuildTools <unconfigured@null.spigotmc.org>
2021-05-21 11:55:25 +0200

    added simple comments

--- src/main/java/de/gueni/gcoins/GCoinsPlugin.java
+++ src/main/java/de/gueni/gcoins/GCoinsPlugin.java
@@ -18,19 +18,24 @@ public class GCoinsPlugin extends JavaPlugin {
     }
 
     private void setup() {
+        // Checking if CoinAPI is not enabled, if yes we disable the plugin
         if ( !getServer().getPluginManager().getPlugin( "CoinAPI" ).isEnabled() ) {
             getLogger().log( Level.WARNING, "§cGCoins requires CoinAPI to work.. Disabling plugin!" );
             getServer().getPluginManager().disablePlugin( this );
             return;
         }
+
+        // Saving default config and initializing configHandler
         saveDefaultConfig();
         configHandler = new ConfigHandler( this );
 
+        // Registering commands
         new AddCoinsCommand( getConfigHandler().getMessage( "commands.add_coins.permission" ), getConfigHandler().getNotPermitted(), this );
         new RemoveCoinsCommand( getConfigHandler().getMessage( "commands.remove_coins_permission" ), getConfigHandler().getNotPermitted(), this );
         new SetCoinsCommand( getConfigHandler().getMessage( "commands.set_coins_permission" ), getConfigHandler().getNotPermitted(), this );
         new CoinsCommand( getConfigHandler().getMessage( "commands.coins.permission" ), getConfigHandler().getNotPermitted(), this );
         new PayCommand( getConfigHandler().getMessage( "commands.pay_permission" ), getConfigHandler().getNotPermitted(), this );
 
+        getLogger().log( Level.FINE, "§aPlugin started successfully!" );
     }
 }
